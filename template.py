37d1686e2e4d9efde1197d6309e7200e5a93f7fc
seaify <dilin.life@gmail.com>
2015-03-22 22:17:24 -0400

    first version

---
+++ template.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import request, render_template
+
+
+app = Flask(__name__)
+app.debug = True
+
+Template_dict = {
+    '1': 'shadowsocks-client.json'
+}
+
+@app.route('/get_hello', methods = ['GET'])
+def get_hello():
+    print "hello"
+    return "hello"
+
+@app.route('/get_template', methods = ['POST'])
+def get_template():
+    post_data = {
+        'template_id': request.form.get('template_id'),
+        'server': request.form.get('server'),
+        'password': request.form.get('password'),
+    }
+    return render_template(Template_dict[post_data['template_id']], **post_data)
+
+if __name__ == "__main__":
+    app.run()
