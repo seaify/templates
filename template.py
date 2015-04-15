55ef26a42f44b495f5fd05668cb52ad6768a104f
seaify <dilin.life@gmail.com>
2015-04-15 03:12:07 -0400

    remove hard code, add nginx-flask.json

--- template.py
+++ template.py
@@ -8,21 +8,13 @@ app.debug = True
 
 Template_dict = {
     '1': 'shadowsocks-config.json',
+    '2': 'nginx-flask.json',
 }
-
-@app.route('/get_hello', methods = ['GET'])
-def get_hello():
-    print "hello"
-    return "hello"
 
 @app.route('/get_template', methods = ['POST'])
 def get_template():
     print request.form
-    post_data = {
-        'template_id': request.form.get('template_id'),
-        'server': request.form.get('server'),
-        'password': request.form.get('password'),
-    }
+    post_data = dict((key, request.form.getlist(key) if len(request.form.getlist(key)) > 1 else request.form.getlist(key)[0]) for key in request.form.keys())
     return render_template(Template_dict[post_data['template_id']], **post_data)
 
 if __name__ == "__main__":
